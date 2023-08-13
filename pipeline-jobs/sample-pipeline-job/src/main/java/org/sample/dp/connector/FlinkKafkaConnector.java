2798d53b321f5c4e4f6669da811e6f9af79ace09
shiva-rakshith <rakshiths@sanketika.in>
2023-08-13 17:31:46 +0530

    feat: update flink

--- pipeline-jobs/sample-pipeline-job/src/main/java/org/sample/dp/connector/FlinkKafkaConnector.java
+++ pipeline-jobs/sample-pipeline-job/src/main/java/org/sample/dp/connector/FlinkKafkaConnector.java
@@ -1,28 +1,16 @@
 package org.sample.dp.connector;
 
-import org.apache.flink.api.common.serialization.DeserializationSchema;
-import org.apache.flink.api.common.serialization.SerializationSchema;
-import org.apache.flink.streaming.api.datastream.DataStreamSink;
-import org.apache.flink.streaming.api.datastream.DataStreamSource;
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
-import org.apache.flink.streaming.connectors.kafka.KafkaSerializationSchema;
-import org.apache.flink.streaming.connectors.kafka.KafkaDeserializationSchema;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.sample.dp.serde.MapDeserializationSchema;
-import org.sample.dp.serde.MapSerializationSchema;
 import org.sample.dp.serde.StringDeserializationSchema;
-import org.sample.dp.serde.StringSerializationSchema;
 import org.sample.dp.task.SampleJobConfig;
 
-import java.util.Map;
-import java.util.Optional;
 import java.util.Properties;
-import org.apache.flink.api.common.serialization.SimpleStringSchema;
 
 public class FlinkKafkaConnector {
 
@@ -38,14 +26,6 @@ public class FlinkKafkaConnector {
 
     public SinkFunction<String> kafkaStringSink(String kafkaTopic) {
         return new FlinkKafkaProducer<>(kafkaTopic, new SimpleStringSchema(), getKafkaProducerProperties());
-    }
-
-    public SourceFunction<Map<String,Object>> kafkaMapSource(String kafkaTopic) {
-        return new FlinkKafkaConsumer<>(kafkaTopic, new MapDeserializationSchema() , getKafkaConsumerProperties());
-    }
-
-    public SinkFunction<Map<String, Object>> kafkaMapSink(String kafkaTopic) {
-        return new FlinkKafkaProducer<>(kafkaTopic, new MapSerializationSchema(kafkaTopic, Optional.empty()), getKafkaProducerProperties(), FlinkKafkaProducer.Semantic.AT_LEAST_ONCE);
     }
 
     public Properties getKafkaConsumerProperties() {
